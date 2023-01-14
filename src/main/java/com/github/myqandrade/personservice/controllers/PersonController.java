f516fb1fbc8ae595355062effca673f0ae166fcb
Mike Andrade <mike.andrade.oliveira@gmail.com>
2023-01-13 21:18:35 -0300

    Creating find person by id

--- src/main/java/com/github/myqandrade/personservice/controllers/PersonController.java
+++ src/main/java/com/github/myqandrade/personservice/controllers/PersonController.java
@@ -1,7 +1,6 @@
 package com.github.myqandrade.personservice.controllers;
 
 import com.github.myqandrade.personservice.model.Person;
-import com.github.myqandrade.personservice.repositories.AddressRepository;
 import com.github.myqandrade.personservice.repositories.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/persons")
@@ -17,13 +17,19 @@ public class PersonController {
     @Autowired
     private PersonRepository personRepository;
 
-    @Autowired
-    private AddressRepository addressRepository;
-
     @GetMapping
     public ResponseEntity<List<Person>> findAll(){
         return ResponseEntity.status(HttpStatus.OK
         ).body(personRepository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Person> findById(@PathVariable Integer id){
+        Optional<Person> person = personRepository.findById(id);
+        if(person.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(person.get());
     }
 
     @PostMapping
--- src/main/java/com/github/myqandrade/personservice/repositories/AddressRepository.java
+++ src/main/java/com/github/myqandrade/personservice/repositories/AddressRepository.java
@@ -4,8 +4,6 @@ import com.github.myqandrade.personservice.model.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
-
 @Repository
-public interface AddressRepository extends JpaRepository<Address, Long> {
+public interface AddressRepository extends JpaRepository<Address, Integer> {
 }
